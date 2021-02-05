039dc2e8f887afb9d5e57f94581a466882ef50b4
thiago5171 <tgazaroli@gmail.com>
2021-02-05 16:43:43 -0200

    Update cmds1.go

--- cmds1.go
+++ cmds1.go
@@ -17,7 +17,7 @@ func main() { // essa func main mostra onde sera o inicio da execução do codig
 	//a fmt println retorna 2 valores, ou seja eh nescesario 2 variaveis  para  colocar 2 valores, mas caso eu nao queira
 	//um desses valores eu posso  colocar um _ no local que seria para uma variavel receber o valor, entao simplesmente nao atribui
 	//o resultado retornado a ninguem
-	// OBS: EU N USEI := NESTE CASO POIS SO SE PODE ATRIBUIR APOS TER FEITO UMA DECLARAÇÃO EM UMA VARIAVEL
+	// OBS: EU N USEI := NESTE CASO POIS SO SE PODE ATRIBUIR APOS TER FEITO UMA DECLARAÇÃO EM UMA VARIAVEL, isso é explicado melhor no cmds2.go
 	_ , erros = fmt.Println("heelo world", "ola pessoas", "demontrar numeros 100 e ", 100)
 	fmt.Println(erros)
 

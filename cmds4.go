400bbeae650b65ec1a0dc28be635511bd287344d
thiago5171 <tgazaroli@gmail.com>
2021-02-18 05:17:19 -0200

    Update cmds4.go

--- cmds4.go
+++ cmds4.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main(){
+	/*
+	existem 3 tipos de variaçoes de print
+	1)
+	aquela que possui 
+	-printf que na sua estrutura vai ter primeiro o texto nas aspas duplas e depoi vai ter uma virgula e as variaveis que serao encaixadas dentro das aspas
+	fmt.Printf("%s is %d years old.\n", name, age)
+	-prinln exibe  e pula uma linha
+	fmt.Println(name, "is", age, "years old.")
 	
+	2)
+	*/
+
+	
+	/*
+	
+	*/
 }

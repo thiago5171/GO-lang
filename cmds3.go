11c5be20467103e99b974ccf97cd0a2f56d3c205
thiago5171 <tgazaroli@gmail.com>
2021-02-18 05:17:14 -0200

    Update cmds3.go

--- cmds3.go
+++ cmds3.go
@@ -23,10 +23,18 @@ func main(){
 	nome := "thiago"
 	fmt.Printf("%v tem %v anos de idade \n",nome, idade)
 	// lembrando que se a variavel nao estiver sendo usada o programa nao vai rodar
-
-	// em go quando atribuimos um tipo primitivo em uma variavel não é possivel trocar esse tipo posteriormente
+	//----------------------------------------------------------------------------------------------------
+	// em GO quando atribuimos um tipo primitivo em uma variavel não é possivel trocar esse tipo posteriormente
+	//----------------------------------------------------------------------------------------------------
 	//so se pode fazer uma atribuição para tal variavel sem falar o tipo primitivo (ou seja uma declaração) 
 	//dentro de uma  func main(){}
+	//----------------------------------------------------------------------------------------------------
+	//usando o /t dentro das aspas teremos um TAB nesse espaço
+	//para aparecer as aspas no texo tem que usar contra barra antes \" texto desejado \"
+	//----------------------------------------------------------------------------------------------------
+	//caso queira que mostre um texto CRU renm nenhuma interpretação , basta colocar set texto dentro de acentos grave
+	// `texto sem formatação `
+
 	
 	
 }

643074ce73a805e747f2ce0cb26105d8f9aa00c0
thiago5171 <tgazaroli@gmail.com>
2021-02-19 20:26:47 -0200

    Create ex01.go

---
+++ exercicios/ex01.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+func main(){
+	x :=42
+	y := "james bond"
+	z :=  true
+	fmt.Printf("x = %v, y = %v, z = %v\n",x,y,z)
+	//multiplas linhas
+	fmt.Printf("x = %v\n",x)
+	fmt.Printf("y = %v\n",y)
+	fmt.Printf("z = %v\n",z)
+	
+
+
+
+} 	

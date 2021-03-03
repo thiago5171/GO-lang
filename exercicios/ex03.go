54e8e89ba3dac6f190fe70cd37caf9b3e1ff85c9
thiago5171 <tgazaroli@gmail.com>
2021-03-03 17:38:28 -0300

    Update ex03.go

--- exercicios/ex03.go
+++ exercicios/ex03.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 )
-
+var x = 42
 var y = "james bond"
 var z = true
 
 func main(){
 	
-	s := fmt.Sprintf(y,z)
+	s := fmt.Sprintf("%v %v %T",x,y,z)
 	fmt.Println(s)
 
 }

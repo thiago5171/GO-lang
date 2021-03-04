f387ec5e38fb25cc0c5f797a8e44d103c4431d42
thiago5171 <tgazaroli@gmail.com>
2021-03-04 15:55:02 -0300

    Update ex05.go

--- exercicios/ex05.go
+++ exercicios/ex05.go
@@ -3,6 +3,16 @@ package main
 import (
 	"fmt"
 )
+type inteiro int
+var x  inteiro
+var y int
 func main(){
-	
+	fmt.Printf("a variaval x tem valor = %v\ne tipo subjancente =%T\n",x,x)
+	x = 42
+	fmt.Printf("%v\n",x)
+	// METODO PARA CONVERTER UM TIPO int()
+	y = int(x)
+	fmt.Printf("a variaval y tem valor = %v\ne tipo  =%T\n",y,y)
+
 }
+

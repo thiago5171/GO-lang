39d88996120f72c0a07b06eaa36e48c7301ca872
thiago5171 <tgazaroli@gmail.com>
2021-03-05 18:12:11 -0300

    Update cmds5.go

--- cmds5.go
+++ cmds5.go
@@ -6,12 +6,13 @@ import (
 
 // para criar um tipo basta fazer neste modelo abaixo,sendo int seu tipo subjacente
 type hotdog int
+
 //para atribuir o tipo a uma variavel
-var b hotdog =6
+var b hotdog = 6
 
-func main(){
+func main() {
 
-	fmt.Printf("%T\n",b)
+	fmt.Printf("%T\n", b)
 	fmt.Println(b)
 	//nao é possivel fazer isso, porque estaria fazendo operaçoes ou atribuiçoes com tipos diferentes
 	//soma = 5+b
@@ -19,4 +20,17 @@ func main(){
 	// mas é possivel caso converta  o b para virar do mesmo tipo que o valor somado
 	var soma = 5 + int(b)
 	fmt.Println(soma)
+
+	soma1 := fmt.Sprintf("%v", soma)
+	// caso queira converter em bytes algum elemento  basta fazer o seguinte
+
+	var bytes = []byte(soma1)
+	fmt.Printf("quantidade de bytes usasdo foi =%v \n", bytes)
+
+	// para exibir um texto da mesma forma que esta sendo digitada em um progrmaa basta utilizar ` texto desejado`
+	s := `pipipip0i
+		tste 
+			texte2`
+	fmt.Printf("%v",s)
+
 }

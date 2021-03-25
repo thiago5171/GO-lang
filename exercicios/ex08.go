48bf1f944e62b57ed198f8658067e0709ae6c0a3
thiago5171 <Tgazaroli@gmail.com>
2021-03-25 02:26:13 -0300

    Update ex08.go

--- exercicios/ex08.go
+++ exercicios/ex08.go
@@ -3,8 +3,23 @@ package main
 import (
 	"fmt"
 )
-
+var x int
 func main(){
+/*x=1
+for x<=10000{
+	fmt.Printf("%v-",x)
+	x++
+}
+*/
+//OU
+x=1
+for {
+	fmt.Printf("%v-",x)
+	x++
+	if x==10000{
+		break
+	}
+}
 
 
 }

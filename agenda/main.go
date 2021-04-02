820bcc372da263f607ae189f83a9116b3e5a47a7
thiago5171 <Tgazaroli@gmail.com>
2021-04-02 18:36:12 -0300

    Update main.go

--- agenda/main.go
+++ agenda/main.go
@@ -1,15 +1,35 @@
-package main
+qpackage main
 
 import (
 	"fmt"
 )
 
+type Contato struct {
+	Nome string
+	Telefone string
+	Email string
+}
+
+func (c Contato) VerContato()  {
+	
+	fmt.Println(c.Nome)
+	
+}
+//  a definião do objeto seria em um arquivo main separado, e oque esta abaixo é colocado emoutro arquivo
 func main(){
-	thiago := &Contato{
+	thiago := Contato{
 		Nome : "thiago",
 		Telefone : "9999-9999",
 		Email : "tgazaroli@gmail.com",
 	}
 
-	fmt.Printf(thiago)
+	fmt.Println(thiago.Telefone)
 }
+
+
+
+
+/*
+reader := bufio.NewReader(os.Stdin)
+text, _ := reader.ReadString('\n')
+*/

ffb5534d99116dea3db0ba4694ed8905bcabe541
thiago5171 <Tgazaroli@gmail.com>
2021-04-04 20:45:44 -0300

    Create cmds12.go

---
+++ cmds12.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+)
+func amin(){
+	
+fmt.Println("USO DO MAKE ABAIXO")
+
+//'MAKE'
+//A ESTRUTURA DO MAKE É A SEGUINTE
+
+slice3[0]=8
+slice3= append(slice3 ,1)
+
+fmt.Println(slice)
+
+fmt.Println(len(slice))
+
+fmt.Println(cap(slice))
+
+
+}

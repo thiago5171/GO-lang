f97956662bd2c547f54d2abc7dcafd5a93c0ecaf
thiago5171 <Tgazaroli@gmail.com>
2021-04-03 21:14:41 -0300

    Update ex18.go

--- exercicios/ex18.go
+++ exercicios/ex18.go
@@ -1,10 +1,14 @@
 package main
-
+// tente acessar todos os itens de uma slice *sem* utilizar range.
 import (
 	"fmt"
 )
 
 func main(){
 
+	s :=[]int{1,2,3,4,5}
 
+	for i:=0;i< len(s);i++{
+ 		fmt.Println(s[i])
+	} 
 }
